616e587cead3309ef53a6cb67258d5aa72672851
AlexGonzales-xd <alexjonasgs107@gmail.com>
2026-04-10 13:20:27 -0500

    xd

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,20 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    alert("Bienvenido a Gotagota");
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
+document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:8080/api/clientes')
         .then(response => response.json())
         .then(data => {
-            console.log('Data:', data);
-            const elemento = document.getElementById("tabla-clientes");
-            data.forEach(cliente => {
-                const row = document.createElement("tr");
-                row.innerHTML = `
-                    <td>${cliente.id}</td>
-                    <td>${cliente.nombre}</td>
-                    <td>${cliente.email}</td>
-                `;
-                elemento.appendChild(row);
-            });
-      
-            
-        });
+            console.log('Datos:', data);
+            const elemento = document.getElementById("tabla-cliente")
+            elemento.innerHTML = JSON.stringify(data);
+            console.log(elemento)
+        })
 });
