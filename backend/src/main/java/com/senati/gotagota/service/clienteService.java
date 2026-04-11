6c0cdcc334bf6be8421e799caf289486405859a7
AlexGonzales-xd <alexjonasgs107@gmail.com>
2026-04-11 14:12:27 -0500

    xd

--- backend/src/main/java/com/senati/gotagota/service/clienteService.java
+++ backend/src/main/java/com/senati/gotagota/service/clienteService.java
@@ -3,6 +3,8 @@ package com.senati.gotagota.service;
 import com.senati.gotagota.entity.Cliente;
 import com.senati.gotagota.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 import java.util.List;
 //Importamos la anotacion @service
 //Esto es la capa de la logica de negocio, aqui van las validaciones, calculos, etc.
@@ -10,13 +12,22 @@ import java.util.List;
 public class ClienteService {
     //Inyectamos el repositorio para poder acceder a la base de datos
     private final ClienteRepository clienteRepository;
+    private final ResponseEntityExceptionHandler responseEntityExceptionHandler;
 
     //Contructor: Spring inyecta automaticamente el repositorio(las dependencias)
-    public ClienteService(ClienteRepository clienteRepository) {
+    public ClienteService(ClienteRepository clienteRepository, ResponseEntityExceptionHandler responseEntityExceptionHandler) {
         this.clienteRepository = clienteRepository;
+        this.responseEntityExceptionHandler = responseEntityExceptionHandler;
     }
+
     //Retorna o recive la lista de todos los clientes
-    public List<Cliente> listarTodos(){
+    public List<Cliente> listarTodos() {
         return clienteRepository.findAll();
+    }
+
+    // Elimina el cliete por ID
+    public void eliminarCliente(Long id) {
+        clienteRepository.deleteAllById(id);
+        return responseEntity.noContent().build();
     }
 }
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,9 +3,32 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:8080/api/clientes')
         .then(response => response.json())
         .then(data => {
-            console.log('Datos:', data);
-            const elemento = document.getElementById("tabla-cliente")
-            elemento.innerHTML = JSON.stringify(data);
-            console.log(elemento)
+            const elemento = document.getElementById("table-cliente")
+
+            for (let i = 0; i < data.length; i++) {
+                const cliente = data[i]
+                const row = document.createElement("tr")
+                row.innerHTML = `
+                    <td>${cliente.id}</td>
+                    <td>${cliente.nombre}</td>
+                    <td>${cliente.apellido}</td>
+                    <td>${cliente.dni}</td>
+                    <td>${cliente.telefono}</td>
+                    <td>${cliente.direccion}</td>
+                    <td>
+                    <button class="btn btn-outline-primary">
+                    <i class="fas fa-edit"></i> Editar
+                    </button>
+
+                    <button class="btn btn-outline-danger">
+                        <i class="fas fa-trash-alt"></i> Eliminar
+                    </button> 
+                </td>
+                `
+                elemento.appendChild(row)
+            }
+
+
+
         })
 });
